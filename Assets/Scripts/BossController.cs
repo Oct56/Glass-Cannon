c06e6573d04c4e1296a313d176ace642fa124a28
David Jones <jonesdav98@gmail.com>
2022-09-22 22:20:31 -0400

    Crouching and Jumping added to character controller

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
 	[SerializeField] private float health = 100f;
-
+	public GameObject boss;
 
 	// Update is called once per frame
 	void Update()
     {
-        
+        if(health == 0)
+        {
+			boss.SetActive(false);
+        }
     }
 
 	public void TakeDamage(int damage)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,45 @@ public class PlayerController : MonoBehaviour
 	private float horizontal;
 	public PlayerProjectile projectilePrefab;
 	public Transform fireOffset;
+
 	private bool isFacingRight = true;
-	[SerializeField] private float shootCooldown = 0.5f;
+
 	private float currentCooldown;
+
+	public Vector2 standingSize;
+	public Vector2 crouchingSize;
+	public Vector2 offsetSize;
+
+	public BoxCollider2D Collider;
 
 	// SerializeField shows the variables in the inspector on Unity which is helpful for debugging.
 	[SerializeField] private Rigidbody2D rb;
 	[SerializeField] private float health = 12f;
 	[SerializeField] private float speed = 500f;
 
+	[SerializeField] public float groundRadius;
+	[SerializeField] private LayerMask ground;
+	[SerializeField] public bool grounded = true;
+	[SerializeField] private float jumpForce;
+
+	[SerializeField] private float shootCooldown = 0.5f;
+
+
+	void Start()
+	{
+		Collider = GetComponent<BoxCollider2D>();
+		standingSize = Collider.size;
+		Collider.size = standingSize;
+	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		//Movement
 		horizontal = Input.GetAxisRaw("Horizontal"); //Returns -1 if the input is A and 1 if the input is D 
 		rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); // Moves the player on the x-axis by taking a keyboard input and multipying it by the speed variable.
 
+		//Shooting
 		if (Input.GetKeyDown(KeyCode.J) && currentCooldown <= 0) //If J is pressed while shooting isn't on cooldown...
 		{
 			currentCooldown = shootCooldown;
@@ -35,7 +58,34 @@ public class PlayerController : MonoBehaviour
 			currentCooldown -= Time.deltaTime;
 		}
 
+		//Jumping
+		grounded = Physics2D.OverlapCircle(rb.position, groundRadius, ground); //Draws a circle around the player taking in the player's position and ground radius as input. The circle checks to see if a LayerMask of type ground is touching it.
+
+		if (Input.GetButtonDown("Jump") && grounded)
+		{
+			Jump();
+		}
+
+		//Crouching
+		if (Input.GetKeyDown(KeyCode.S)) //Crouches when S button is held
+		{
+			Collider.size = crouchingSize;
+			Collider.offset = offsetSize;
+		}
+
+		if (Input.GetKeyUp(KeyCode.S)) //Uncrouches when S is no longer held who would have thought.
+		{
+			Collider.size = standingSize;
+			Collider.offset = new Vector2(0, 0);
+		}
+
 		Flip();
+	}
+
+	private void Jump()
+	{
+		grounded = false;
+		rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 	}
 
 	private void Flip()
